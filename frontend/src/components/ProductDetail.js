fb3593f07692d1d6487f3b3ae4929fda77c143c3
Jonathan <78221006+JSiv92@users.noreply.github.com>
2023-04-29 21:46:07 +1200

    finished get all products with basic styling

---
+++ frontend/src/components/ProductDetail.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ProductDetails = ({ product }) => {
+  return (
+    <div className="product-details">
+      <h4>{product.name}</h4>
+      <p>
+        <strong>Price: </strong>$ {product.price}
+      </p>
+      <p>Description: {product.description}</p>
+      <p>{product.createdAt}</p>
+    </div>
+  );
+};
+
+export default ProductDetails;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,36 @@
 //blank react component for home page
+import { useEffect, useState } from "react";
 import React from "react";
 
+//components
+import ProductDetails from "../components/ProductDetail";
+
 const Home = () => {
+  const [products, setProducts] = useState(null);
+
+  //useeffect hook fires function ONCE when component is rendered (empty array)
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch("/api/products");
+      const json = await response.json();
+
+      //check response is ok
+      if (response.ok) {
+        setProducts(json);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <div className="Home">
-      <h2>Home Page Content</h2>
+      <div className="products">
+        {products &&
+          products.map((product) => (
+            <ProductDetails key={product._id} product={product} />
+          ))}
+      </div>
     </div>
   );
 };
