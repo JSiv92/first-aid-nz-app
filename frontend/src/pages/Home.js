4cd4a66b0c8fdc2407b7ef8c9274d7e245c47ed3
Jonathan <78221006+JSiv92@users.noreply.github.com>
2023-04-29 17:13:19 +1200

    create navbar and add some styling

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+//blank react component for home page
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="Home">
+      <h2>Home Page Content</h2>
+    </div>
+  );
+};
+
+export default Home;
