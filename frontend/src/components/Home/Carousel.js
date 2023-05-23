4b5e9920b61e891897c2de7f2e5d4b046ab1f8d6
Jonathan <78221006+JSiv92@users.noreply.github.com>
2023-05-23 20:39:38 +1200

    setup grid for homepage

--- frontend/src/components/Home/Carousel.js
+++ frontend/src/components/Home/Carousel.js
@@ -1,48 +1,45 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Carousel from "react-bootstrap/Carousel";
 import "./Carousel.css";
 import medics from "../../assets/events.png";
-import drugs from "../../assets/drugscreening.png";
 import health from "../../assets/healthscreening.png";
 import courses from "../../assets/courses.png";
 
 const Slides = () => {
   return (
     <Carousel>
-      <Carousel.Item interval={6000}>
+      <Carousel.Item interval={9000}>
         <div className="tintDiv">
-          <img
-            className="slide fluid d-block w-100"
-            src={medics}
-            alt="Event Medics"
-          />
+          <Link to="/courses">
+            <img className="slide fluid w-100" src={courses} alt="Courses" />
+          </Link>
         </div>
         <Carousel.Caption></Carousel.Caption>
       </Carousel.Item>
+
       <Carousel.Item interval={6000}>
         <div className="tintDiv">
-          <img
-            className="slide fluid w-100"
-            src={courses}
-            alt="Courses"
-            to="/courses"
-          />
+          <Link to="/medics">
+            <img
+              className="slide fluid w-100"
+              src={medics}
+              alt="Event Medics"
+            />
+          </Link>
         </div>
         <Carousel.Caption></Carousel.Caption>
       </Carousel.Item>
+
       <Carousel.Item interval={6000}>
         <div className="tintDiv">
-          <img className="slide fluid w-100" src={drugs} alt="Drug Screening" />
-        </div>
-        <Carousel.Caption></Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item interval={6000}>
-        <div className="tintDiv">
-          <img
-            className="slide fluid d-block w-100"
-            src={health}
-            alt="Health Screening"
-          />
+          <Link to="/screening">
+            <img
+              className="slide fluid w-100"
+              src={health}
+              alt="Health Screening"
+            />
+          </Link>
         </div>
         <Carousel.Caption></Carousel.Caption>
       </Carousel.Item>
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,5 +1,8 @@
 //blank react component for home page
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 //components
 import Carousel from "../components/Home/Carousel.js";
@@ -10,35 +13,32 @@ const Home = () => {
     <>
       <Carousel />
       <div className="page">
-        <div className="content">
-          <h2 style={{ color: "white" }}>Home Page</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eget
-            rutrum leo. Etiam mi urna, tempor ut maximus at, accumsan eget
-            sapien. Nulla sollicitudin massa id eros tempus tincidunt. Duis vel
-            augue ut velit auctor cursus ut non risus. Nulla mollis lectus et
-            mauris vestibulum pulvinar. In nisi arcu, vestibulum eu bibendum
-            tempor, suscipit vel tortor. Donec nibh ante, malesuada non luctus
-            at, dictum ut turpis. Mauris et est leo. Praesent vitae pulvinar
-            magna, sed ornare felis. In non erat eget massa <br /> brfeugiat
-            sagittis eget vitae lacus. In sagittis, risus in facilisis dapibus,
-            felis sem dictum turpis, eget lacinia ipsum metus in nisl. Nullam
-            fringilla, elit vel pretium ultrices, massa nibh euismod libero, ac
-            interdum quam mi in nunc. Praesent id consequat purus. Vivamus
-            malesuada luctus quam, nec aliquam nisi ultrices quis. Nulla id eros
-            et libero pulvinar consequat. Nunc tempus rhoncus mauris consequat
-            vestibulum. Etiam a euismod lorem. Phasellus pretium purus erat,
-            quis gravida risus dapibus ac. Suspendisse potenti. Integer at
-            finibus augue. Nulla eleifend libero in pretium laoreet. Aliquam
-            faucibus diam sed
-            <br /> neque ullamcorper, ut aliquam libero accumsan. Donec
-            tristique urna ex, in bibendum dolor placerat tincidunt. Maecenas
-            hendrerit vestibulum fermentum. Proin auctor dui a tincidunt
-            dignissim. Donec sed aliquet arcu. Quisque malesuada luctus
-            lobortis. Proin eu ante vitae lacus posuere fermentum ut sit amet
-            purus. Phasellus egestas eleifend purus, <br />
-          </p>
-        </div>
+        <Container>
+          <Row>
+            <Col sm={12} md={6} lg={3}>
+              1.Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+            </Col>
+            <Col sm={12} md={6} lg={3}>
+              2.Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+            </Col>
+            <Col sm={12} md={6} lg={3}>
+              3.Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+            </Col>
+            <Col sm={12} md={6} lg={3}>
+              4.Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+            </Col>
+          </Row>
+        </Container>
       </div>
     </>
   );
